157275740b43cba1d1ba85fb0893e40cc05e148c
Yash-Kakadiya <136950942+Yash-Kakadiya@users.noreply.github.com>
2025-02-18 11:01:16 +0530

    Add files via upload

---
+++ Lab-11/calc.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return a + b
+
+def subs(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def div(a,b):
+    if b == 0:
+        return "Error: Division by zero is not allowed"
+    return a / b
